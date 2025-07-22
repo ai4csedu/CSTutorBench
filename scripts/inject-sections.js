3668eaa2d14b1126aeb17b7f76f4285ff4345879
Zekai1122 <90919515+Zekai1122@users.noreply.github.com>
2025-07-22 16:41:11 +1000

    Refactored the code structure and added three new sections: download, authors, and contact.

---
+++ scripts/inject-sections.js
@@ -0,0 +1,16 @@
+async function loadSection(id, url) {
+  const res = await fetch(url);
+  const html = await res.text();
+  document.getElementById(id).innerHTML = html;
+}
+
+// load each section
+loadSection("hero-container", "sections/hero/hero.html");
+loadSection("about-container", "sections/about/about.html");
+loadSection("dataset-container", "sections/dataset/dataset.html");
+loadSection("evaluation-container", "sections/evaluation/evaluation.html");
+loadSection("leaderboard-container", "sections/leaderboard/leaderboard.html");
+loadSection("footer-container", "sections/footer/footer.html");
+loadSection("authors-container", "sections/authors/authors.html");
+loadSection("contact-container", "sections/contact/contact.html");
+loadSection("download-container", "sections/download/download.html");
